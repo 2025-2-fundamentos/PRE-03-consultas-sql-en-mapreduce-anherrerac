bd91e4347eb954fb0608003742433f148c1acc8a
Andres Herrera Correa <anherrerac@unal.edu.co>
2025-10-01 10:42:22 -0500

    sln

--- homework/mapreduce.py
+++ homework/mapreduce.py
@@ -10,7 +10,7 @@ def mapreduce(input_folder, output_folder, mapper_fn, reducer_fn):
             with open(file, "r", encoding="utf-8") as f:
                 for line in f:
                     sequence.append((file, line))
-        return sequence	
+        return sequence
 
     def save_results_to_output(result):
         with open(f"{output_folder}/part-00000", "w", encoding="utf-8") as f:
@@ -34,8 +34,4 @@ def mapreduce(input_folder, output_folder, mapper_fn, reducer_fn):
     create_output_directory(output_folder)
     save_results_to_output(result)
     create_success_file(output_folder)
-
-
-
-
-
+    
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,132 +1,31 @@
-"""Taller evaluable"""
+"""Autograding script."""
 
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
 
-from homework.mapreduce import mapreduce  # type: ignore
-import shutil
 import os
-#
-# Columns:
-# total_bill, tip, sex, smoker, day, time, size
-#
 
-#
-# QUERY 1:
-# SELECT *, tip/total_bill as tip_rate
-# FROM tips;
-#
-
-def mapper_query_1(sequence):
-    """Mapper"""
-    result = []
-    for index, (_, row) in enumerate(sequence):
-        if index == 0:
-            result.append(
-                (index, row.strip() + ",tip_rate")
-            )
-        else:
-            row_values = row.strip().split(",")
-            total_bill = float(row_values[0])
-            tip = float(row_values[1])
-            tip_rate = tip / total_bill
-            result.append((index, row.strip() + "," + str(tip_rate)))
-    return result
+from homework.queries import run
 
 
-def reducer_query_1(sequence):
-    """Reducer"""
-    return sequence
-
-
-#
-# SELECT *
-# FROM tips
-# WHERE time = 'Dinner';
-#
-def mapper_query_2(sequence):
-    """Mapper"""
-    result = []
-    for index, (_, row) in enumerate(sequence):
-        if index == 0:
-            result.append((index, row.strip()))
-        else:
-            row_values = row.strip().split(",")
-            if row_values[5] == "Dinner":
-                result.append((index, row.strip()))
-    return result
-
-
-def reducer_query_2(sequence):
-    """Reducer"""
-    return sequence
-
-#
-# SELECT *
-# FROM tips
-# WHERE time = 'Dinner' AND tip > 5.00;
-#
-def mapper_query_3(sequence):
-    """Mapper"""
-    result = []
-    for index, (_, row) in enumerate(sequence):
-        if index == 0:
-            result.append((index, row.strip()))
-        else:
-            row_values = row.strip().split(",")
-            if row_values[5] == "Dinner" and float(row_values[1]) > 5.00:
-                result.append((index, row.strip()))
-    return result
-
-
-def reducer_query_3(sequence):
-    """Reducer"""
-    return sequence
-
-
-#
-# ORQUESTADOR:
-#
-def run():
-    """Orquestador"""
-
-    if os.path.exists("files/query_1"):
-            shutil.rmtree("files/query_1")
-
-    mapreduce(
-        input_folder="files/input/", 
-        output_folder="files/query_1", 
-        mapper_fn=mapper_query_1, 
-        reducer_fn=reducer_query_1,
-    )
-
-
-    if os.path.exists("files/query_2"):
-        shutil.rmtree("files/query_2")
-
-    mapreduce(
-        input_folder="files/input/", 
-        output_folder="files/query_2", 
-        mapper_fn=mapper_query_2, 
-        reducer_fn=reducer_query_2,
-    )    
-
-    if os.path.exists("files/query_3"):
-        shutil.rmtree("files/query_3")
-
-    mapreduce(
-        input_folder="files/input/", 
-        output_folder="files/query_3", 
-        mapper_fn=mapper_query_3, 
-        reducer_fn=reducer_query_3,
-    )    
-
-
-if __name__ == "__main__":
+def test_01():
+    """Test the homework."""
 
     run()
 
-    
-    
-    
-    
+    for directory in [
+        "files/query_1/",
+        "files/query_2/",
+        "files/query_3/",
+        "files/query_4/",
+        "files/query_5/",
+    ]:
+        for file in [
+            "_SUCCESS",
+            "part-00000",
+        ]:
+
+            filename = directory + file
+
+            if not os.path.exists(filename):
+                raise Exception(f"The file {filename} does not exist")
